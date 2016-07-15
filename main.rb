d7a3341b4d1196fbafd4619734e5417e1fd493fd
Kassandre Pedro <kpedro@mailjet.com>
2016-07-15 17:10:18 +0200

    little modif

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'time'
 puts "Welcome in treasure-chest !"
 
 # How long before the trip starts ?
-puts "So now tell me when do your trip start ? (use yyyy/mm/dd)"
+puts "So now tell, me when do your trip start ? (use yyyy/mm/dd)"
 date1 = gets.to_s
 date2 = Date.today.to_s
 a = Date.parse(date1)
@@ -13,35 +13,35 @@ b = Date.parse(date2)
 monthsLeft = (a.mjd - b.mjd)/30
 
 # how much we need and currency
-puts "First what is your currency ?"
+puts "What is your currency ?"
 currency = gets
 puts "How much do you wanna save ?"
 howMuch = gets.to_i
 tripMoneySaving = howMuch / monthsLeft
 
-puts "Okay so you've got " + monthsLeft.to_s + "month(s) to get " + howMuch.to_s + " " + currency + "!"
+puts "So you've got " + monthsLeft.to_s + "month(s) to get " + howMuch.to_s + " " + currency + "!"
 
 # calculs
-puts "Okay now let's if it's possible or not to save until the : " + date1
+puts "Okay, now let's see if it's possible or not for you to save that muchh until the : " + date1
 # positive
 puts "How much do you get paid ?"
 paid = gets.to_i
 puts "If you got extra money how much ?"
 extra = gets.to_i
-puts "Some student loan ?"
+puts "Some government's help ?"
 help = gets.to_i
 positive = paid + extra + help
-print "In one moth you can gather: " + positive.to_s + " " + currency
+print "In one month you can gather: " + positive.to_s + " " + currency
 # negative
-puts "Now let's see the wrong side"
+puts "Now let's go on the dark side ..."
 puts "How much is your rent (with electricity etc) ?"
 rent = gets.to_i
-puts "How much do you use for food ?"
+puts "How much do you use for food (be honest!) ?"
 food = gets.to_i
-puts "Count all the extras (sports, fun, party)"
+puts "Count all the extras (sports, fun, party, pokemons ...)"
 extras = gets.to_i
 
 negative = rent + food + extras + tripMoneySaving
 rest = positive - negative
 # results
-print "So in one month remains : " +  rest.to_s + " " + currency + "after saving money has been taken !"
+print "So in one month remains : " +  rest.to_s + " " + currency + "after your saving-money-trip has been taken !"
